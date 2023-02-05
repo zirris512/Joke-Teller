82d49d06ff74b81e4fa36c0ad9209126e6f73888
zirris512 <zirris512@gmail.com>
2023-02-05 15:20:08 -0600

    add disable button while joke is being told

--- events.js
+++ events.js
@@ -2,6 +2,7 @@ import { textToSpeech } from "./textToSpeech.js";
 
 // Get jokes from joke api
 export async function getJokes() {
+    toggleButton(this);
     let joke;
     const JokeUrl =
         "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
@@ -18,4 +19,8 @@ export async function getJokes() {
     } catch (err) {
         console.error(err);
     }
+}
+
+export function toggleButton(button) {
+    button.disabled = !button.disabled;
 }
--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-import { getJokes } from "./getJokes.js";
+import { getJokes, toggleButton } from "./events.js";
 
 const button = document.querySelector("#button");
+const audio = document.querySelector("#audio");
+
+audio.addEventListener("ended", () => toggleButton(button));
 
 button.addEventListener("click", getJokes);
